0576af7fcc798ce83c8c57030538599201499ee9
wahtej <125274697+wahtej@users.noreply.github.com>
2023-06-01 13:06:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ provider "aws" {
 
 module "vpc_subnet_nsg" {
  source = "./modules/vpc_subnet_nsg"
- vpc_cidr_block = "10.0.0.0/16"
- subnet_cidr_block = "10.0.1.0/24"
+ vpc_cidr_block = var.vpc_cidr_block
+ subnet_cidr_block = var.subnet_cidr_block
  security_group_name = "my-security-group"
 }
 
@@ -19,5 +19,5 @@ module "ec2_instance" {
  subnet_id = module.vpc_subnet_nsg.subnet_id
  security_group_id = module.vpc_subnet_nsg.security_group_id
  instance_type = "t2.medium"
- ami = "ami-02eb7a4783e7e9317"
+ ami = "var.ami"
 }
